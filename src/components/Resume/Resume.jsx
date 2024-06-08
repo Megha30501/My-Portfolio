344dc49bafb7e9e8a650fa4f259fa55384096fbf
Megha30501 <m_patel39@u.pacific.edu>
2024-06-07 17:33:24 -0700

    solved the error of resume displaying

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -5,9 +5,12 @@ import Particle from "../Particle";
 import pdf from "../../Assets/Megha_Patel_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+  ).toString();
 
 function Resume() {
   const [width, setWidth] = useState(1200);
