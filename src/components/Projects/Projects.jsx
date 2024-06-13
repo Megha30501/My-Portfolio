db35c5cdf27bc366cc3840e43013a165d7adae58
Megha30501 <m_patel39@u.pacific.edu>
2024-06-13 16:00:02 -0700

    made logo

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,72 +2,76 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import ProjectCard from "./ProjectCard";
-import leaf from "../../Assets/Projects/leaf.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import poster from "../../Assets/Projects/poster.png";
+import dashboard from "../../Assets/Projects/dashboard.png";
+import AA from "../../Assets/Projects/Academic-Advantage.png";
+import toy from "../../Assets/Projects/toy.jpg";
+import visa from "../../Assets/Projects/visa.jpg";
 
 function Projects() {
-    return (
-      <Container fluid className="project-section">
-        <Particle />
-        <Container>
-          <h1 className="project-heading">
-            My Recent <strong className="purple">Works </strong>
-          </h1>
-          <p style={{ color: "white" }}>
-            Here are a few projects I've worked on recently.
-          </p>
-          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={chatify}
-                isBlog={false}
-                title="Academic Advantage"
-                // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-                description="Academic Advantage Is A React Application Facilitating Student Access To Teaching Assistants' Availability And Location Information Managed By The University's Student Support Center."
-                ghLink="https://github.com/Megha30501/Academic-Advantages"
-                demoLink="https://academic-advantage.onrender.com/"
-              />
-            </Col>
-  
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={bitsOfCode}
-                isBlog={false}
-                title="Bits-0f-C0de"
-                description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-                ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-                demoLink="https://blogs.soumya-jit.tech/"
-              />
-            </Col>
-  
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={editor}
-                isBlog={false}
-                title="H1b Visa Prediction"
-                description="Using Logistic Regression, Created Predictive Model For Forecasting H1B Visa Approval Likelihoods For Applicants."
-                ghLink="https://github.com/Megha30501/H1b-Visa-Prediction"              
-              />
-            </Col>
-  
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={leaf}
-                isBlog={false}
-                title="Toy Rental"
-                description="Toy Rental, A Web Application Offering Toy Purchase Or Rental Options For Children And Categorized Into Packages."
-                ghLink="https://github.com/Megha30501/Toy-Rental"
-              />
-            </Col>
-  
-          </Row>
-        </Container>
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={poster}
+              isBlog={false}
+              title="Resource Constraints Intrusion Detection Framework for VANET"
+              description="Developed a lightweight Machine Learning (ML) framework for intrusion detection in Vehicular Ad-Hoc Networks (VANETs), utilizing Principal Component Analysis (PCA) for dimensionality reduction. This optimized framework significantly enhances VANET security by achieving high detection performance while minimizing resource consumption."
+              ghLink="https://scholarlycommons.pacific.edu/rcs/2024/undergraduate/8/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={dashboard}
+              isBlog={false}
+              title="Finance Dashboard"
+              description="Created a streamlined Personal Finance Dashboard using React,chart.js for the frontend, Node.js/Express for the backend, and MongoDB for data storage. This dashboard allows users to track expenses and incomes, featuring a summary view of total income, expenses, and net balance, alongside a dynamic expense breakdown chart. Users can add, edit, and delete transactions with ease, supported by RESTful API endpoints for efficient data management."
+              ghLink="https://github.com/Megha30501/Finance-Dashboard"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={AA}
+              isBlog={false}
+              title="Academic Advantage"
+              description="Created a React application to provide 24/7 access to Teaching Assistant (TA) information from the university's Student Support Center database. Implemented features for locating TA availability and contact details, and developed RESTful web services using Node.js for seamless integration with a MongoDB database, ensuring efficient data retrieval and real-time updates."
+              ghLink="https://github.com/Megha30501/Academic-Advantages"
+              demoLink="https://academic-advantage.onrender.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={visa}
+              isBlog={false}
+              title="H1b Visa Prediction"
+              description="Developed a predictive model using machine learning algorithms, including logistic regression and random forest, to forecast H1B visa application outcomes with an 87% accuracy rate. Analyzed applicant demographics, job details, and employer characteristics to offer insights into the H1B visa application process for employers and foreign workers."
+              ghLink="https://github.com/Megha30501/H1b-Visa-Prediction"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={toy}
+              isBlog={false}
+              title="Toy Rental"
+              description="Built an e-commerce website using PHP, MySQL, and HTML/CSS, offering options to rent or buy toys. Integrated an online payment gateway with Google Pay, achieving a 90% success rate in test transactions, and enhanced user experience by streamlining the rental and purchase process for customers."
+              ghLink="https://github.com/Megha30501/Toy-Rental"
+            />
+          </Col>
+        </Row>
       </Container>
-    );
-  }
-  
-  export default Projects;
-  
+    </Container>
+  );
+}
+
+export default Projects;
